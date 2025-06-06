44510101391807e0ed4e4f3b26f14191290382b0
Ferdhika Yudira <rpl4rt08@gmail.com>
2025-06-06 21:37:09 +0700

    :tada: fix unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './contexts/ThemeContext';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import RoomsSection from './components/RoomsSection';
-import AboutSection from './components/AboutSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
-import AdminPanel from './components/AdminPanel';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import RoomsSection from "./components/RoomsSection";
+import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+import AdminPanel from "./components/AdminPanel";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function MainSite() {
   return (
@@ -29,13 +28,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<MainSite />} />
-          <Route 
-            path="/admin" 
+          <Route
+            path="/admin"
             element={
               <ProtectedRoute requireAdmin={true}>
                 <AdminPanel />
               </ProtectedRoute>
-            } 
+            }
           />
         </Routes>
       </Router>
